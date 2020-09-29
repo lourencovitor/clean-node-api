38ed3d422e1e6969af60c8e853e6ed189a26e543
Vitor Lourenço <50245199+lourencovitor@users.noreply.github.com>
2020-09-28 22:33:46 -0300

    ensure LoginRouter returns 400 if no password ins provided

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'teste123'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if not password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'vitor@teste.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
